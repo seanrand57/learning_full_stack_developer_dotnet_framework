97f9e0a06c212052f157710c22945589c10e1716
Sean Rand <seanrand57@hotmail.com>
2020-07-23 10:27:27 -0400

    Updating GigController and ViewModel

--- learning_full_stack_developer_dotnet_framework/Controllers/GigsController.cs
+++ learning_full_stack_developer_dotnet_framework/Controllers/GigsController.cs
@@ -1,10 +1,13 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Web.Mvc;
 using learning_full_stack_developer_dotnet_framework.Models;
 using learning_full_stack_developer_dotnet_framework.ViewModels;
+using Microsoft.AspNet.Identity;
 
 namespace learning_full_stack_developer_dotnet_framework.Controllers
 {
+    [Authorize]
     public class GigsController : Controller
     {
         private readonly ApplicationDbContext _context;
@@ -21,6 +24,23 @@ namespace learning_full_stack_developer_dotnet_framework.Controllers
                 Genres = _context.Genres.ToList()
             };
             return View(viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Create(GigFormViewModel viewModel)
+        {
+            var gig = new Gig
+            {
+                ArtistId = User.Identity.GetUserId(),
+                DateTime = DateTime.Parse($"{viewModel.Date} {viewModel.Time}"),
+                GenreId = viewModel.Genre,
+                Venue = viewModel.Venue
+            };
+
+            _context.Gigs.Add(gig);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index","Home");
         }
     }
 }
--- learning_full_stack_developer_dotnet_framework/Models/Gig.cs
+++ learning_full_stack_developer_dotnet_framework/Models/Gig.cs
@@ -7,12 +7,18 @@ namespace learning_full_stack_developer_dotnet_framework.Models
     {
         public int Id { get; set; }
 
-        [Required] public ApplicationUser Artist { get; set; }
+        public ApplicationUser Artist { get; set; }
+
+        // Foreign Key Property
+        [Required] public string ArtistId { get; set; }
 
         public DateTime DateTime { get; set; }
 
         [Required] [StringLength(255)] public string Venue { get; set; }
 
-        [Required] public Genre Genre { get; set; }
+        public Genre Genre { get; set; }
+
+        // Foreign Key Property
+        [Required] public int GenreId { get; set; }
     }
 }
