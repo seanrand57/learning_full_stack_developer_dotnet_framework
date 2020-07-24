6cc1ba15f402db0b4beb384b9e35ea1b275011fd
Sean Rand <seanrand57@hotmail.com>
2020-07-24 15:06:37 -0400

    Removing casade entity framework deletes from model fro gig-> attendees

--- learning_full_stack_developer_dotnet_framework/Models/ApplicationDbContext.cs
+++ learning_full_stack_developer_dotnet_framework/Models/ApplicationDbContext.cs
@@ -8,6 +8,7 @@ namespace learning_full_stack_developer_dotnet_framework.Models
 
         public DbSet<Gig> Gigs { get; set; }
         public DbSet<Genre> Genres { get; set; }
+        public DbSet<Attendance> Attendances { get; set; }
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
@@ -16,6 +17,16 @@ namespace learning_full_stack_developer_dotnet_framework.Models
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Attendance>()
+                .HasRequired(a => a.Gig)
+                .WithMany()
+                .WillCascadeOnDelete(false);
+
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
